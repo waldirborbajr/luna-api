24ca21084fe7a5d1de46c2c8beb336a653d32d85
Waldir Borba Junior <wborbajr@gmail.com>
2025-02-05 16:26:13 -0300

    Update api.php

--- apiv2/api.php
+++ apiv2/api.php
@@ -1,5 +1,6 @@
 <?php
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");//this allows cors
+header('Content-Type: application/json');
 include 'db.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
