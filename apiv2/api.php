b3e9b80a31119ba39eebe55dead7e9fc708628d6
Waldir Borba Junior <wborbajr@gmail.com>
2025-02-06 06:34:46 -0300

    Update api.php

--- apiv2/api.php
+++ apiv2/api.php
@@ -1,6 +1,10 @@
 <?php
+header('Cache-Control: no-cache, no-store, max-age=0, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+header('Pragma: no-cache');
 header("Access-Control-Allow-Origin: *");//this allows cors
-header('Content-Type: application/json');
+header("Content-type: application/json; charset=utf-8");
+
 include 'db.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -29,6 +33,7 @@ function handleGet($pdo) {
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    http_response_code(200);
     echo json_encode($result);
 }
 
@@ -51,5 +56,37 @@ function handleDelete($pdo, $input) {
     $stmt = $pdo->prepare($sql);
     $stmt->execute(['id' => $input['id']]);
     echo json_encode(['message' => 'User deleted successfully']);
+}
+
+/*
+ * returnJsonHttpResponse
+ * @param $success: Boolean
+ * @param $data: Object or Array
+ */
+function returnJsonHttpResponse($httpCode, $data)
+{
+    // remove any string that could create an invalid JSON 
+    // such as PHP Notice, Warning, logs...
+    ob_start();
+    ob_clean();
+
+    // this will clean up any previously added headers, to start clean
+    header_remove(); 
+
+    // Set the content type to JSON and charset 
+    // (charset can be set to something else)
+    // add any other header you may need, gzip, auth...
+    header("Content-type: application/json; charset=utf-8");
+
+    // Set your HTTP response code, refer to HTTP documentation
+    http_response_code($httpCode);
+    
+    
+    // encode your PHP Object or Array into a JSON string.
+    // stdClass or array
+    echo json_encode($data);
+
+    // making sure nothing is added
+    exit();
 }
 ?>
