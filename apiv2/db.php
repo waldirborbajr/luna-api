65f39f1cb69ab4afcdadf1e5776b043ab98c4988
Waldir Borba Junior <wborbajr@gmail.com>
2025-02-06 14:22:01 -0300

    Update db.php

--- apiv2/db.php
+++ apiv2/db.php
@@ -13,7 +13,7 @@ $dbname = "users";
 // }
 
 try {
-	$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass, [
+	$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass, [
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
