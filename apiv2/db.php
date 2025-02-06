5b1c66ebb4ad2dacbea3e051034c6535ea2b1339
Waldir Borba Junior <wborbajr@gmail.com>
2025-02-06 08:23:46 -0300

    Update db.php

--- apiv2/db.php
+++ apiv2/db.php
@@ -4,11 +4,21 @@ $user = "root";
 $password = "";
 $dbname = "users";
 
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+//     $pdo->exec("set names utf8");
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     die("Connection failed: " . $e->getMessage());
+// }
+
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-    $pdo->exec("set names utf8");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass, [
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+    ]);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
-}
+}    
 ?>
